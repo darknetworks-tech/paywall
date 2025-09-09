5d9e420fa5f86d779fe34882579f339912771e74
Oparah Samuel <sammy.nisa821@gmail.com>
2025-09-09 11:40:29 +0100

    Initial commit

---
+++ src/main/java/com/sif/paywall/PaywallApplication.java
@@ -0,0 +1,13 @@
+package com.sif.paywall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaywallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaywallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sif/paywall/PaywallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sif.paywall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaywallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
